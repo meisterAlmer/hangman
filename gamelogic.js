60272b44e8c289c992a169dc2c62c36ce9a7aabb
Almer <48512743+meisterAlmer@users.noreply.github.com>
2021-01-28 20:37:07 +0100

    first commit

---
+++ gamelogic.js
@@ -0,0 +1,26 @@
+function displayWordSoFar(word, guesses) {
+  // - [ ] zet je testje op test.only (later weer weghalen)
+  // - [ ] loggen: wat is word, wat is guesses
+  // - [ ] javascript, ['a', 'b'] -> "_ _ _ _ _ _ _ _ _ _ "
+  // - [ ] javascript "splitten" split() -> ['j','a','v','a','s','c','r','i','p','t']
+  // - [ ] een loop maken -> ['j','a','v','a','s','c','r','i','p','t']
+  // - [ ] checken: zit de letter in guesses?
+  // - [ ] if else
+  // - [ ] zo nee: "_ "
+  // - [ ] zo ja: "j " (niet j, maar de letter die we dan checken)
+  // - [ ] klaar? -> onze string die we hebben gemaakt returnen
+}
+
+function isGameWon(word, guesses) {
+  // WRITE ME
+}
+
+function isGameLost(word, guesses) {
+  // WRITE ME
+}
+
+module.exports = {
+  displayWordSoFar: displayWordSoFar,
+  isGameWon: isGameWon,
+  isGameLost: isGameLost,
+};
